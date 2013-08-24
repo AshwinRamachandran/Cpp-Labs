1956398befca24f56a26f739f513fbd7b8a0235a
Ashwin Ramachandran <ashwinram@server.fake>
2013-08-24 01:24:09 -0700

    Completed Lab2 exercise

---
+++ Cpp/Lab2/Matrix.cpp
@@ -0,0 +1,90 @@
+#include "Matrix.h"
+
+// Constructors
+
+Matrix::Matrix() {
+    num_rows = 0;
+    num_cols = 0;
+
+    mat = new int[1];
+}
+
+Matrix::Matrix(int r, int c) {
+    num_rows = r;
+    num_cols = c;
+
+    mat = new int[num_rows * num_cols];
+}
+
+Matrix::Matrix(Matrix &m) {
+    num_rows = m.getrows();
+    num_cols = m.getcols();
+
+    mat = new int[num_rows * num_cols];
+
+    for (int i = 0; i < num_rows; i++) {
+        for (int j = 0; j < num_cols; j++) {
+            mat[(i * num_cols) + j] = m.getelem(i, j);
+        }
+    }
+}
+
+// Destructor
+Matrix::~Matrix() {
+    delete[] mat;
+}
+
+// Accessors
+int Matrix::getrows() {
+    return num_rows;
+}
+
+int Matrix::getcols() {
+    return num_cols;
+}
+
+int Matrix::getelem(int r, int c) {
+    return mat[(r * num_cols) + c];
+}
+
+// Mutators
+void Matrix::setelem(int r, int c, int v) {
+    mat[(r * num_cols) + c] = v;
+}
+
+// Member functions
+void Matrix::add(Matrix &m) {
+    if ((num_rows != m.getrows()) || (num_cols != m.getcols()))
+        return;
+
+    for (int i = 0; i < num_rows; i++) {
+        for (int j = 0; j < num_cols; j++) {
+            mat[(i * num_cols) + j] += m.getelem(i, j);
+        }
+    }
+}
+
+void Matrix::subtract(Matrix &m) {
+    if ((num_rows != m.getrows()) || (num_cols != m.getcols()))
+        return;
+
+    for (int i = 0; i < num_rows; i++) {
+        for (int j = 0; j < num_cols; j++) {
+            mat[(i * num_cols) + j] -= m.getelem(i, j);
+        }
+    }
+}
+
+bool Matrix::equals(Matrix &m) {
+    if ((num_rows != m.getrows()) || (num_cols != m.getcols()))
+        return false;
+
+    for (int i = 0; i < num_rows; i++) {
+        for (int j = 0; j < num_cols; j++) {
+            if (mat[(i * num_cols) + j] != m.getelem(i, j))
+                return false;
+        }
+    }
+
+    return true;
+}
---
+++ Cpp/Lab2/Matrix.h
@@ -0,0 +1,41 @@
+/*
+ * A matrix manipulation class
+ *
+ * Author: Ashwin Ramachandran
+ */
+
+#ifndef __MATRIX_H__
+#define __MATRIX_H__
+
+#include <cmath>
+
+class Matrix {
+ public:
+    // Constructors
+    Matrix();
+    Matrix(int r, int c);
+    Matrix(Matrix &m);
+
+    // Destructor
+    ~Matrix();
+
+    // Accessors
+    int getrows();
+    int getcols();
+    int getelem(int r, int c);
+
+    // Mutators
+    void setelem(int r, int c, int v);
+
+    // Member functions
+    void add(Matrix &m);
+    void subtract(Matrix &m);
+    bool equals(Matrix &m);
+
+ private:
+    int *mat;
+
+    int num_rows;
+    int num_cols;
+};
+#endif
